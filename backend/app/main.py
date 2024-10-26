7fa36b8740fb455234f437c42061b34643c9ed56
Roma <romantkacenko82@gmail.com>
2024-10-26 13:27:20 +0300

    Change main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,22 @@
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="API Task Manager")
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(
