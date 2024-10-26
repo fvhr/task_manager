1791e34d45accff8ad5c18e3d4d5f8683cff0d1d
Roma <romantkacenko82@gmail.com>
2024-10-26 17:18:01 +0300

    Settings

---
+++ backend/app/settings/config.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass, field
+
+from environs import Env
+
+env = Env()
+env.read_env()
+
+
+@dataclass
+class Settings:
+    DB_USER: str = field(default_factory=lambda: env("DB_USER"))
+    DB_PASS: str = field(default_factory=lambda: env("DB_PASS"))
+    DB_HOST: str = field(default_factory=lambda: env("DB_HOST"))
+    DB_PORT: str = field(default_factory=lambda: env("DB_PORT"))
+    DB_NAME: str = field(default_factory=lambda: env("DB_NAME"))
+
+
+
